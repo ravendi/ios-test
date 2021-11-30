0cb3c8fcfa5e6b0dce2d10ecdf09678a13463309
Damian Danowski <damian.danowski@miquido.com>
2021-11-30 01:20:01 +0100

    Init project

---
+++ TestWithFlutter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWithFlutter
+//
+//  Created by Damian Danowski on 30/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
